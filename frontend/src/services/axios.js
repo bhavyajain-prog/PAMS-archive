715a52b7c8f829ff11bc92743151a51043a8c2e9
Bhavya Jain <jainbhavya2306@gmail.com>
2025-11-03 20:33:39 +0530

    environement variables

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://project-allocation-process-and-evaluation.onrender.com",
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
